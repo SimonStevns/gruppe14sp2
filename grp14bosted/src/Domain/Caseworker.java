4f4cd19cb799953002d8fdaac65416b1864a13a1
A-Kvisgaard <andreas@kvisgaard.biz>
2019-04-05 12:09:11 +0200

    Create Caseworker.java

---
+++ grp14bosted/src/Domain/Caseworker.java
@@ -0,0 +1,22 @@
+package Domain;
+
+/**
+ *
+ * @author Andreas Kvisgaard | akvis18@student.sdu.dk
+ */
+public class Caseworker 
+{
+    //Atributes
+    int foo = 0;
+    
+    //Constructors
+
+    public Caseworker() 
+    {
+        
+    }
+
+    //Methods
+
+    
+}
