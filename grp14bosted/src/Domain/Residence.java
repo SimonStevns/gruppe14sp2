441d8cb77a944f339d041eee69b20e5ebb1a33fd
Amsalk99 <43138821+Amsalk99@users.noreply.github.com>
2019-04-09 10:39:08 +0200

    Added residence and updated resident

---
+++ grp14bosted/src/Domain/Residence.java
@@ -0,0 +1,20 @@
+
+package Domain;
+
+public class Residence {
+     //Atributes
+    private String name, phoneNumber, email;
+    
+    //Constructors
+
+    public Residence(String name, String phoneNumber, String email) 
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    //Methods
+
+    
+}
