1b4b5420d5a49ab854dc59698610be9e17cd64a0
Simon Klemmen Stevns Olsen <simol18@student.sdu.dk>
2019-04-09 09:18:40 +0200

    controller for main page created

---
+++ grp14bosted/src/gr14bosted/MainFXMLController.java
@@ -0,0 +1,14 @@
+package gr14bosted;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MainFXMLController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
