99312798f68a0e5677df9e37590235792257ac08
nicobah <42782542+nicobah@users.noreply.github.com>
2019-05-26 18:51:04 +0200

    More tests

--- grp14bosted/test/Domain/PassCheckerTest.java
+++ grp14bosted/test/Domain/PassCheckerTest.java
@@ -12,9 +12,14 @@ public class PassCheckerTest {
     @Test
     public void testCheckPassword() {
         String passwordTrue = "ThisPassw0rdIsLegit";
-        String passwordFalse = "ThisPasswordShouldBeFalse";
+        String passwordNoNum = "ThisShouldBeFalse";
+        String passwordNoUpper = "thissh0uldbefalse";
+        String passwordNoLower = "THISSH0ULDBEFALSE";
         assertTrue(checkPassword(passwordTrue));
-        assertFalse(checkPassword(passwordFalse));
+        assertFalse(checkPassword(passwordNoNum));
+        assertFalse(checkPassword(passwordNoUpper));
+        assertFalse(checkPassword(passwordNoLower));
+        
         
         
         
